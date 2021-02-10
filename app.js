ddfd5600f52dcb72d752fb4ad1f0abfe1545835e
Kashslesh <46224990+Kashslesh@users.noreply.github.com>
2021-02-10 17:39:32 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
 "use strict";
-const btn = document.querySelector("button");
-const inputInfo = document.querySelector("input");
-// console.log(btn, inputInfo);
-let errorCathch; 
-
-  let url = `https://places-dsn.algolia.net/1/places/query`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        btn.addEventListener('click', function(){
-            // console.log(inputInfo.value);
-            data.hits.forEach(element => {
-                
-            });
-
-            // data.hits.forEach(cityPLusGeo => {
-            //     try { 
-                    
-            //         // for(let i = 0;i< data.hits.length; i++ );
-            //         // console.log(cityPLusGeo.city[inputInfo.value]);
-            //         // console.log(cityPLusGeo.city.default[0]);
-            //         // console.log(cityPLusGeo._geoloc.lat);
-            //         // console.log(cityPLusGeo._geoloc.lng);
-            //     }catch (errorCathch){
-
-            //     };
-
-            // });
-        });
-    })
-    .catch((err) => {
-      // fonction pour gérer les erreurs
-      console.log("mon erreur est :" + err); // si erreur(s) j'affiche laquelle en console
-    });
-
-
-
-
+ let oke
 // On initialise la latitude et la longitude de Marselle (centre de la carte)
 let mymap = L.map("mapid").setView([43.2944724, 5.3601373], 16);
 L.tileLayer(
@@ -49,3 +12,51 @@ L.tileLayer(
   }
 ).addTo(mymap);
 let marker = L.marker([43.2944724, 5.3601373], 16).addTo(mymap);
+
+
+
+
+const btn = document.querySelector("button");
+const inputInfo = document.querySelector("input");
+// console.log(btn, inputInfo);
+
+
+
+let errorCathch; 
+  let url = `https://places-dsn.algolia.net/1/places/query`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        btn.addEventListener('click', function(){
+            // console.log(inputInfo.value);
+            let found = false;
+            data.hits.forEach(function(elem) {              
+                if(elem.locale_names.default[0].toLowerCase().includes(inputInfo.value.toLowerCase())){
+                  found =true;
+                }
+              });
+              if(found == true){
+                console.log("trouve")
+    
+              }else {
+                console.log("pas trouve")
+              }
+              
+            });
+          })
+    .catch((err) => {
+      // fonction pour gérer les erreurs
+      console.log("mon erreur est :" + err); // si erreur(s) j'affiche laquelle en console
+    });
+
+
+
+
+
+
+
+
+
+
+
+
