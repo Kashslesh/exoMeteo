fa4f5b3ed1e3f67e37c4a46199c1ca3c206d43af
Kashslesh <46224990+Kashslesh@users.noreply.github.com>
2021-02-11 17:49:44 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,86 @@
 "use strict";
- let oke
-// On initialise la latitude et la longitude de Marselle (centre de la carte)
-let mymap = L.map("mapid").setView([43.2944724, 5.3601373], 16);
-L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
-  {
-    maxZoom: 18,
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-  }
-).addTo(mymap);
-let marker = L.marker([43.2944724, 5.3601373], 16).addTo(mymap);
-
-
-
+const getMap = function(){
+  // On initialise la latitude et la longitude de Marselle (centre de la carte)
+  let mymap = L.map("mapid").setView([43.2944724, 5.3601373], 16);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    {
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+    }
+  ).addTo(mymap);
+  let marker = L.marker([43.2944724, 5.3601373], 16).addTo(mymap);
+};
+getMap();
 
 const btn = document.querySelector("button");
 const inputInfo = document.querySelector("input");
+const divResult = document.querySelector(".result")
+
+ 
+// console.log(divResult);
 // console.log(btn, inputInfo);
 
-
-
-let errorCathch; 
-  let url = `https://places-dsn.algolia.net/1/places/query`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        btn.addEventListener('click', function(){
-            // console.log(inputInfo.value);
-            let found = false;
-            data.hits.forEach(function(elem) {              
-                if(elem.locale_names.default[0].toLowerCase().includes(inputInfo.value.toLowerCase())){
-                  found =true;
-                }
-              });
-              if(found == true){
-                console.log("trouve")
-    
-              }else {
-                console.log("pas trouve")
-              }
-              
-            });
-          })
-    .catch((err) => {
-      // fonction pour gérer les erreurs
-      console.log("mon erreur est :" + err); // si erreur(s) j'affiche laquelle en console
+///// Autocompletion
+function autoComplet(letter){
+  
+  divResult.innerHTML = "";
+  fetch("https://places-dsn.algolia.net/1/places/query", {
+  method: "POST",
+  body: JSON.stringify({ query: inputInfo.value, hitsPerPage: '3' })
+  })
+  .then(response => response.json())
+  .then((data) => {
+    // console.log(data);
+    data.hits.forEach((elem) => {
+      // console.log(elem);
+      
+       let result = (elem.locale_names.default[0]);
+        const resultDeCherch = `
+        <li>${result}</li>`
+      //  console.log(resultDeCherch);
+       divResult.insertAdjacentHTML("beforeend", resultDeCherch);
+      
     });
+    const listBl = document.querySelectorAll('li');
+    console.log(listBl);
+    
+    //// creer un function pour choissir un element de li
+  });
+};
+inputInfo.addEventListener("keyup", () => {
+  autoComplet(inputInfo.value);
+
+} )
+
+
+
+// const meteoFun = function(){
+//   fetch(`api.openweathermap.org/data/2.5/weather?q=${}&appid=2521e64289208e681d392934a06cd2cd`, {
+//     method: "GET",
+//     body: JSON.stringify({ query: inputInfo.value, hitsPerPage: '3' })
+//   })
+//   .then(response => response.json())
+//   .then((dataMeteo) => {
+//     console.log(dataMeteo); // Look at local_names.default
+//   });
+  
+// };
+
+// const searchAlgoliaPlaces = (event) => {
+//   fetch("https://places-dsn.algolia.net/1/places/query", {
+//   method: "POST",
+//   body: JSON.stringify({ query: inputInfo.value })
+//   })
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data)
+//   });
+  
+//  };
+//  btn.addEventListener("keyup", searchAlgoliaPlaces);
 
 
 
